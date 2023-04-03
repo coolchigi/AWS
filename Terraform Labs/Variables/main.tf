ca7bebe31ec39ace6e095a72160a5236101cd54d
Chigo <coolchigi0031@gmail.com>
2023-04-03 15:41:10 -0400

    Attempted to fix server connection error

--- Terraform Labs/Variables/main.tf
+++ Terraform Labs/Variables/main.tf
@@ -3,12 +3,13 @@ resource "aws_instance" "webserver" {
   instance_type          = var.type
   vpc_security_group_ids = [aws_security_group.instance_sg.id]
 
-  user_data = <<-EOF
+  user_data                   = <<-EOF
               #!/bin/bash
-              echo "Hello, World" > index.html
-              nohup busybox httpd -f -p ${var.server_port} &
-              EOF
-
+             echo "Hello, World" > index.html
+             sudo yum -y update
+             sudo yum -y install httpd
+             sudo service httpd start
+            EOF
   user_data_replace_on_change = true
 
 
@@ -19,13 +20,44 @@ resource "aws_instance" "webserver" {
 }
 
 resource "aws_security_group" "instance_sg" {
-  name        = "MyEC2instance"
+  name        = "instance_sg"
   description = "Enable SSH and HTTP access"
+
   ingress {
     from_port   = var.server_port
     to_port     = var.server_port
     protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  #Allow incoming TCP requests on port 22 from any IP
+  ingress {
+    description = "Incoming SSH"
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  #Allow incoming TCP requests on port 443 from any IP
+  ingress {
+    description = "Incoming 443"
+    from_port   = 443
+    to_port     = 443
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  #Allow all outbound requests
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "instance_sg"
   }
 
 }
