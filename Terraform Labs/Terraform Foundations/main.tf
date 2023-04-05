c8b9faf8acaf0c540ce3ab3e9520a945cfecbc0e
Chigo <coolchigi0031@gmail.com>
2023-04-05 11:05:42 -0400

    Terraform foundations

---
+++ Terraform Labs/Terraform Foundations/main.tf
@@ -0,0 +1,19 @@
+resource "aws_vpc" "myvpc"{
+    cidr_block = "10.123.0.0/16"
+    enable_dns_hostnames = true
+    enable_dns_support = true
+
+    tags = {
+        Name = "dev"
+    }
+}
+
+resource "aws_subnet" "my_public_subnet"{
+    vpc_id = aws_vpc.myvpc.id
+    cidr_block = "10.123.1.0/24"
+    map_public_ip_on_launch = true
+    availability_zone = "us-east-1e"
+    tags = {
+        Name = "dev_public"
+    }
+}
