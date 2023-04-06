77491b1ca6182f38c77e6d7efa5fd87bed5711ba
Chigo <coolchigi0031@gmail.com>
2023-04-05 23:26:42 -0400

    Made changes to main.tf file

--- Terraform Labs/Terraform Foundations/main.tf
+++ Terraform Labs/Terraform Foundations/main.tf
@@ -1,19 +1,27 @@
-resource "aws_vpc" "myvpc"{
-    cidr_block = "10.123.0.0/16"
-    enable_dns_hostnames = true
-    enable_dns_support = true
+resource "aws_vpc" "myvpc" {
+  cidr_block           = "10.123.0.0/16"
+  enable_dns_hostnames = true
+  enable_dns_support   = true
 
-    tags = {
-        Name = "dev"
-    }
+  tags = {
+    Name = "dev"
+  }
 }
 
-resource "aws_subnet" "my_public_subnet"{
-    vpc_id = aws_vpc.myvpc.id
-    cidr_block = "10.123.1.0/24"
-    map_public_ip_on_launch = true
-    availability_zone = "us-east-1e"
-    tags = {
-        Name = "dev_public"
-    }
+resource "aws_subnet" "my_public_subnet" {
+  vpc_id                  = aws_vpc.myvpc.id
+  cidr_block              = "10.123.1.0/24"
+  map_public_ip_on_launch = true
+  availability_zone       = "us-east-1e"
+  tags = {
+    Name = "dev_public"
+  }
+}
+
+resource "aws_internet_gateway" "my_internet_gateway" {
+  vpc_id = aws_vpc.myvpc.id
+
+  tags = {
+    Name = "dev_igw"
+  }
 }
