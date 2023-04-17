afcba83a5c25b04c1626bc4cda960d0f3310b5cd
Chigo <coolchigi0031@gmail.com>
2023-04-16 21:41:49 -0400

    Added route resource with internet gateway id

--- Terraform Labs/Dev Environment/main.tf
+++ Terraform Labs/Dev Environment/main.tf
@@ -39,4 +39,6 @@ resource "aws_route" "default_route"{
   route_table_id = aws_route_table.my_public_rt.id
 
   destination_cidr_block = "0.0.0.0/0"
+
+  gateway_id = aws_internet_gateway.my_internet_gateway.id
 }
