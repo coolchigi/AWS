8978175bbcd7c5310f0a565285b728a23f3da16b
Chigo <coolchigi0031@gmail.com>
2023-04-20 01:13:03 -0400

    Created Lambda method to get view count from DynamoDB table

---
+++ backend/serverless-resume-website/serverless_web/getLambdaFunction/app.py
@@ -0,0 +1,26 @@
+import boto3
+import json
+
+# Connect to the DynamoDB table
+
+dynamodb = boto3.resource('dynamodb')
+table_name = 'visitrosCountTable'
+table = dynamodb.Table(table_name)
+
+# Event is data passed to the function upon execution
+
+def lambda_handler(event, context):
+
+    response = table.get_item(Key={'ID': 'viewCount'})
+    view_count = response['Item']['count']
+
+    return{
+        'statusCode': 200,
+        'headers': {
+            'Access-Control-Allow-Origin': '*',
+            'Access-Control-Allow-Headers': '*',
+            'Access-Control-Allow-Credentials': '*',
+            'Content-Type': 'application/json'
+        },
+        'body': json.dumps(view_count)
+    }
