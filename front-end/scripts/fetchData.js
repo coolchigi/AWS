a77c063eb591c7885be28d84eab47c43dcb2d830
Chigo <coolchigi0031@gmail.com>
2023-05-10 02:14:30 -0400

    feat(index): Added script to fetch data from table

--- front-end/scripts/fetchData.js
+++ front-end/scripts/fetchData.js
@@ -1,5 +1,13 @@
 var apiUrl = "https://1i6ummbb99.execute-api.us-east-1.amazonaws.com/Prod/counter";
-      fetch(apiUrl
+      fetch(apiUrl , {
+      method: 'PUT',
+      headers: {
+            'Access-Control-Allow-Origin': '*',
+            'Access-Control-Allow-Headers': '*',
+            'Access-Control-Allow-Credentials': '*',
+            'Content-Type': 'application/json'
+      }
+      }
       )
       .then(() => fetch("https://1i6ummbb99.execute-api.us-east-1.amazonaws.com/Prod/counter"))
       .then(response => response.json())
