a98ec456248ba391872f017fc65733732a58e054
Chigo <coolchigi0031@gmail.com>
2023-05-10 02:19:56 -0400

    feat(index): Added script to fetch data from table

--- front-end/scripts/fetchData.js
+++ front-end/scripts/fetchData.js
@@ -1,16 +1,39 @@
+// var apiUrl = "https://1i6ummbb99.execute-api.us-east-1.amazonaws.com/Prod/counter";
+//       fetch(apiUrl , {
+//       method: 'PUT',
+//       headers: {
+//             'Access-Control-Allow-Origin': '*',
+//             'Access-Control-Allow-Headers': '*',
+//             'Access-Control-Allow-Credentials': '*',
+//             'Content-Type': 'application/json'
+//       }
+//       }
+//       )
+//       .then(() => fetch("https://1i6ummbb99.execute-api.us-east-1.amazonaws.com/Prod/counter"))
+//       .then(response => response.json())
+//       .then(data =>{
+//           document.getElementById('visitorNumber').innerHTML = data
+//     console.log(data)});
+
+
 var apiUrl = "https://1i6ummbb99.execute-api.us-east-1.amazonaws.com/Prod/counter";
-      fetch(apiUrl , {
-      method: 'PUT',
-      headers: {
-            'Access-Control-Allow-Origin': '*',
-            'Access-Control-Allow-Headers': '*',
-            'Access-Control-Allow-Credentials': '*',
-            'Content-Type': 'application/json'
-      }
-      }
-      )
-      .then(() => fetch("https://1i6ummbb99.execute-api.us-east-1.amazonaws.com/Prod/counter"))
-      .then(response => response.json())
-      .then(data =>{
-          document.getElementById('visitorNumber').innerHTML = data
-    console.log(data)});
+fetch(apiUrl, {
+  method: 'PUT',
+  headers: {
+    'Content-Type': 'application/json',
+    'Access-Control-Allow-Origin': '*'
+  }
+})
+.then(() => {
+  fetch(apiUrl, {
+    method: 'GET',
+    headers: {
+      'Content-Type': 'application/json',
+      'Access-Control-Allow-Origin': '*'
+    }
+  })
+  .then(response => response.json())
+  .then(data => {
+    document.getElementById('visitorNumber').innerHTML = data
+  });
+});
