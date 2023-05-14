5453dd2d653ba0a64757f1e1ccc1b1eb52921fd2
Chigo <coolchigi0031@gmail.com>
2023-05-13 20:19:21 -0400

    feat(fetchData): Fixed cors error

--- front-end/scripts/fetchData.js
+++ front-end/scripts/fetchData.js
@@ -16,24 +16,45 @@
 //     console.log(data)});
 
 
-var apiUrl = "https://1i6ummbb99.execute-api.us-east-1.amazonaws.com/Prod/counter";
-fetch(apiUrl, {
-  method: 'PUT',
-  headers: {
-    'Content-Type': 'application/json',
-    'Access-Control-Allow-Origin': '*'
-  }
-})
-.then(() => {
-  fetch(apiUrl, {
-    method: 'GET',
-    headers: {
-      'Content-Type': 'application/json',
-      'Access-Control-Allow-Origin': '*'
-    }
-  })
-  .then(response => response.json())
-  .then(data => {
-    document.getElementById('visitorNumber').innerHTML = data
-  });
-});
+function init(){
+  updateData()
+  fetchData()
+}
+
+function updateData(){
+  fetch("https://1i6ummbb99.execute-api.us-east-1.amazonaws.com/Prod/counter")
+}
+function fetchData(){
+   fetch("https://1i6ummbb99.execute-api.us-east-1.amazonaws.com/Prod/counter", )
+    .then((response) => response.json())
+    .then((data) => 
+      document.getElementById('visitorNumber').innerHTML = data
+    );
+}
+
+
+
+init()
+// var apiUrl = "http://127.0.0.1:3000/counter"
+// fetch(apiUrl, {
+//   credentials: 'include',
+//   method: 'PUT',
+//   headers: {
+//     'Content-Type': 'application/json',
+//     'Access-Control-Allow-Origin': '*'
+//   }
+// })
+// .then(() => {
+//   fetch(apiUrl, {
+//     credentials: 'include',
+//     method: 'GET',
+//     headers: {
+//       'Content-Type': 'application/json',
+//       'Access-Control-Allow-Origin': '*'
+//     }
+//   })
+//   .then(response => response.json())
+//   .then(data => {
+//     document.getElementById('visitorNumber').innerHTML = data
+//   });
+// });
