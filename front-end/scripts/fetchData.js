5690bb3b7748ecb87216ab9420ff2ecbd1587f0d
Chigo <coolchigi0031@gmail.com>
2023-05-13 20:23:48 -0400

    feat(fetchData): Updated visitor counter logic

--- front-end/scripts/fetchData.js
+++ front-end/scripts/fetchData.js
@@ -1,21 +1,3 @@
-// var apiUrl = "https://1i6ummbb99.execute-api.us-east-1.amazonaws.com/Prod/counter";
-//       fetch(apiUrl , {
-//       method: 'PUT',
-//       headers: {
-//             'Access-Control-Allow-Origin': '*',
-//             'Access-Control-Allow-Headers': '*',
-//             'Access-Control-Allow-Credentials': '*',
-//             'Content-Type': 'application/json'
-//       }
-//       }
-//       )
-//       .then(() => fetch("https://1i6ummbb99.execute-api.us-east-1.amazonaws.com/Prod/counter"))
-//       .then(response => response.json())
-//       .then(data =>{
-//           document.getElementById('visitorNumber').innerHTML = data
-//     console.log(data)});
-
-
 function init(){
   updateData()
   fetchData()
@@ -35,26 +17,4 @@ function fetchData(){
 
 
 init()
-// var apiUrl = "http://127.0.0.1:3000/counter"
-// fetch(apiUrl, {
-//   credentials: 'include',
-//   method: 'PUT',
-//   headers: {
-//     'Content-Type': 'application/json',
-//     'Access-Control-Allow-Origin': '*'
-//   }
-// })
-// .then(() => {
-//   fetch(apiUrl, {
-//     credentials: 'include',
-//     method: 'GET',
-//     headers: {
-//       'Content-Type': 'application/json',
-//       'Access-Control-Allow-Origin': '*'
-//     }
-//   })
-//   .then(response => response.json())
-//   .then(data => {
-//     document.getElementById('visitorNumber').innerHTML = data
-//   });
-// });
+
