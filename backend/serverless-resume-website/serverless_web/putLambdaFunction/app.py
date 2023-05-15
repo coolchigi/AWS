08cc9b8b0b9fa2eefb0b89beff469d18e569a0c3
Chigo <coolchigi0031@gmail.com>
2023-05-15 12:06:12 -0400

    update function

--- backend/serverless-resume-website/serverless_web/putLambdaFunction/app.py
+++ backend/serverless-resume-website/serverless_web/putLambdaFunction/app.py
@@ -15,12 +15,18 @@ def lambda_handler(event, context):
         ExpressionAttributeValues={':incr': 1},
         ReturnValues='UPDATED_NEW'
     )
+
+    response = table.get_item(Key={'ID': 'viewCount'})
+
+    if "Item" in response:
+                view_count = response['Item']['viewCount']
     return {
         'statusCode': 200,
         'headers': {
             'Access-Control-Allow-Origin': '*',
-            'Access-Control-Allow-Headers': '*',
-            'Access-Control-Allow-Credentials': '*',
-            'Content-Type': 'application/json'
-        }
+            'Access-Control-Allow-Headers': 'Content-Type',
+            'Content-Type': 'application/json',
+            'Access-Control-Allow-Methods': 'OPTIONS,PUT,GET'
+        },
+        "body": view_count
     }
