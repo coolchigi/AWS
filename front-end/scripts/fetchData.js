bcd57da0da1c045825d5d54755e69d59d01f113b
Chigo <coolchigi0031@gmail.com>
2023-05-15 22:32:55 -0400

    feat(fetch): quick url fix

--- front-end/scripts/fetchData.js
+++ front-end/scripts/fetchData.js
@@ -11,7 +11,7 @@ function updateCounter() {
 
 function updateViewCount() {
   const apiUrl = 'https://98obvtmtb1.execute-api.us-east-1.amazonaws.com/Prod/counter';
-  fetch("http://127.0.0.1:3000/counter")
+  fetch(apiUrl)
       .then(res => res.json())
 	    .then(res => {
 		  document.getElementById("visitorNumber").innerHTML = res;
