d4a2fe65b823842f8b869525bc815d0bf0177143
Chigo Eke <coolchigi0031@gmail.com>
2023-11-23 00:19:23 -0500

    feat: Added some changes to the UI

--- front-end/scripts/fetchData.js
+++ front-end/scripts/fetchData.js
@@ -1,20 +1,20 @@
 updateViewCount()
 
-function updateCounter() {
-    fetch("http://127.0.0.1:3000/counter")
-      .then(res => res.json())
-	    .then(res => {
-		  document.getElementById("visitorNumber").innerHTML = res;
-	});
-      
-}
-
 function updateViewCount() {
   const apiUrl = 'https://98obvtmtb1.execute-api.us-east-1.amazonaws.com/Prod/counter';
   fetch(apiUrl)
-      .then(res => res.json())
-	    .then(res => {
-		  document.getElementById("visitorNumber").innerHTML = res;
-	});
-      
+    .then(res => res.json())
+    .then(res => {
+      const visitorNumberElement = document.getElementById("visitorNumber");
+      const visitorCount = parseInt(res);
+      let currentCount = 0;
+      const interval = setInterval(() => {
+        if (currentCount < visitorCount) {
+          currentCount++;
+          visitorNumberElement.textContent = currentCount;
+        } else {
+          clearInterval(interval);
+        }
+      }, 10); // Adjust this value to change the speed of the animation
+    });
 }
