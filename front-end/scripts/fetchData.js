06365647c4832be589d2828ca6a140b556bb25b4
Chigo Eke <coolchigi0031@gmail.com>
2024-01-06 03:34:05 -0500

    feat Update javascript code

--- front-end/scripts/fetchData.js
+++ front-end/scripts/fetchData.js
@@ -1,20 +1,10 @@
 updateViewCount()
-
 function updateViewCount() {
   const apiUrl = 'https://98obvtmtb1.execute-api.us-east-1.amazonaws.com/Prod/counter';
   fetch(apiUrl)
-    .then(res => res.json())
-    .then(res => {
-      const visitorNumberElement = document.getElementById("visitorNumber");
-      const visitorCount = parseInt(res);
-      let currentCount = 0;
-      const interval = setInterval(() => {
-        if (currentCount < visitorCount) {
-          currentCount++;
-          visitorNumberElement.textContent = currentCount;
-        } else {
-          clearInterval(interval);
-        }
-      }, 10); // Adjust this value to change the speed of the animation
-    });
+      .then(res => res.json())
+	    .then(res => {
+		  document.getElementById("visitorNumber").innerHTML = res;
+	});
+      
 }
