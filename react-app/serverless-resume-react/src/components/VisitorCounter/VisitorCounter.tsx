13dd618e0852957caa3a350fb3ea66652af4af8e
coolchigi <coolchigi0031@gmail.com>
2025-07-04 12:47:10 -0400

    feat: implement visitor count fetching from API in VisitorCounter component

--- react-app/serverless-resume-react/src/components/VisitorCounter/VisitorCounter.tsx
+++ react-app/serverless-resume-react/src/components/VisitorCounter/VisitorCounter.tsx
@@ -1,41 +1,29 @@
 import { useState, useEffect } from "react";
-// import { config } from "../../config/config";
+import { config } from "../../config/config";
+
 
 const VisitorCounter = () => {
-  // TODO: Replace with API call later
-  const mockCount = 42;
+  const [visitorCount, setVisitorCount] = useState<number>(0);
+
+  useEffect(() => {
+    const fetchCount = async () => {
+      try {
+        const response = await fetch(config.apiUrl);
+        const data = await response.json();
+        setVisitorCount(data);
+      } catch (error) {
+        console.error("Error fetching visitor count:", error);
+      }
+    };
+
+    fetchCount();
+  }, []);
 
   return (
     <span className="visitor-counter">
-      You are visitor number: <span>{mockCount}</span>
+      You are visitor number: <span>{visitorCount}</span>
     </span>
   );
 };
 
 export default VisitorCounter;
-
-// const VisitorCounter = () => {
-//   const [visitorCount, setVisitorCount] = useState<number>(0);
-
-//   useEffect(() => {
-//     const fetchCount = async () => {
-//       try {
-//         const response = await fetch(config.apiUrl);
-//         const data = await response.json();
-//         setVisitorCount(data);
-//       } catch (error) {
-//         console.error("Error fetching visitor count:", error);
-//       }
-//     };
-
-//     fetchCount();
-//   }, []);
-
-//   return (
-//     <span className="visitor-counter">
-//       You are visitor number: <span>{visitorCount}</span>
-//     </span>
-//   );
-// };
-
-// export default VisitorCounter;
