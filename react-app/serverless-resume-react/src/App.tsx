fd5f00e6cbe17d78de8da75da00eb95bfe380fbe
coolchigi <coolchigi0031@gmail.com>
2026-01-04 21:39:28 -0500

    fix: comment out unused imports and reorder components in App

--- react-app/serverless-resume-react/src/App.tsx
+++ react-app/serverless-resume-react/src/App.tsx
@@ -3,13 +3,13 @@ import "./App.css";
 
 import Header from "./components/Header/Header";
 import About from "./components/About/About";
-import Projects from "./components/Projects/Projects";
+// import Projects from "./components/Projects/Projects";
 import Experience from "./components/Experience/Experience";
-import Education from "./components/Education/Education";
-import Skills from "./components/Skills/Skills";
+// import Skills from "./components/Skills/Skills";
 import Interests from "./components/Interests/Interests";
 import Awards from "./components/Awards/Awards";
 import VisitorCounter from "./components/VisitorCounter/VisitorCounter";
+import TechnicalExpertise from "./components/TechnicalExpertise/TechnicalExpertise";
 
 const App: React.FC = () => {
   return (
@@ -18,11 +18,9 @@ const App: React.FC = () => {
       <main className="main-content">
         <About />
         <Experience />
-        <Projects />
-        <Education />
-        <Skills />
-        <Interests />
+        <TechnicalExpertise />
         <Awards />
+        <Interests />
         <VisitorCounter />
       </main>
     </div>
