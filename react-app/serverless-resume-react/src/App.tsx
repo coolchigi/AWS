fa3b405ec520c785fb5693b617dbbcaf7d6da75e
coolchigi <coolchigi0031@gmail.com>
2026-01-06 10:14:03 -0500

    fix: comment out unused Projects and Skills imports in App component

--- react-app/serverless-resume-react/src/App.tsx
+++ react-app/serverless-resume-react/src/App.tsx
@@ -3,9 +3,7 @@ import "./App.css";
 
 import Header from "./components/Header/Header";
 import About from "./components/About/About";
-// import Projects from "./components/Projects/Projects";
 import Experience from "./components/Experience/Experience";
-// import Skills from "./components/Skills/Skills";
 import Interests from "./components/Interests/Interests";
 import Awards from "./components/Awards/Awards";
 import VisitorCounter from "./components/VisitorCounter/VisitorCounter";
